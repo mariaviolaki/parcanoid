ee5e5e4d2ab8610b3c33b298b9b4a1d57e09fe15
Maria <mariaviolaki@outlook.com>
2020-12-21 18:46:07 +0200

    Start the game with the ball on top of the paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Configuration parameters
+    [SerializeField] Paddle paddle;
+
+    // State
+    float ballToPaddleDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find ball's and paddle's half height
+        float ballHalfHeight = GetComponent<SpriteRenderer>().bounds.extents.y;
+        float paddleHalfHeight = paddle.GetComponent<SpriteRenderer>().bounds.extents.y;
+        // Store the vertical distance between their centers
+        ballToPaddleDistance = ballHalfHeight + paddleHalfHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the ball so that it's on top of the paddle
+        float newYPos = paddle.transform.position.y + ballToPaddleDistance;
+        transform.position = new Vector2(paddle.transform.position.x, newYPos);
+    }
+}
