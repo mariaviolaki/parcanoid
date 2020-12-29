32fb42dd8a6631c3fa28fc0d78cff1dac1257d60
Maria <mariaviolaki@outlook.com>
2020-12-29 17:55:24 +0200

    Continue to next scene after destroying all blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    // Parameters
     [SerializeField] AudioClip breakSound = default;
-    
+
+    // Cached references
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.AddBlock();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
     {
         // Keep playing sound effect even after the block is destroyed
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 0.1f);
+        level.RemoveBlock();
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // Parameters 
+    [SerializeField] int breakableBlocks = 0; // for debugging purposes
+
+    // Cached references
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void AddBlock()
+    {
+        breakableBlocks++;
+    }
+
+    public void RemoveBlock()
+    {
+        breakableBlocks--;
+
+        if (breakableBlocks == 0)
+            sceneLoader.LoadNextScene();
+    }
+}
