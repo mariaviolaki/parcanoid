8d4c50819ded30b92e6c611d8573fa33f048aaa9
Maria <mariaviolaki@outlook.com>
2020-12-29 18:44:23 +0200

    Change timescale for each level

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    // Parameters
+    [Range(0.1f, 5f)] [SerializeField] float gameSpeed = 0.7f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
